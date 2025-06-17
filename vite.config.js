a1fa1b98cd0e397d7b4db743a046991ecf3c3981
thiem ed <dinhthien18052003@gmail.com>
2025-06-17 07:30:42 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000, // Đổi port sang 3000
+    proxy: {
+    '/lms': {
+      target: 'https://13.231.75.96:8443',
+      changeOrigin: true,
+      secure: false
+    }
+  }
   },
 })
