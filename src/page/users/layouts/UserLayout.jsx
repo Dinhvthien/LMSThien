09ce39e2a183da77e5ef45c604a7911d6a0861f8
thiem ed <dinhthien18052003@gmail.com>
2025-07-10 17:39:10 +0700

    123

--- src/page/users/layouts/UserLayout.jsx
+++ src/page/users/layouts/UserLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from '../../users/components/Navbar'; // Adjust path based on your project structure
 import Footer from '../../users/components/Footer'; 
+
 const UserLayout = ({ children }) => {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen flex flex-col bg-gray-100">
       <Navbar />
-      <main className="pt-16">{children}</main>
-      <Footer/>
+      <main className="pt-16 flex-1">{children}</main>
+      <Footer />
     </div>
   );
 };
--- src/page/users/pages/CoursePage.jsx
+++ src/page/users/pages/CoursePage.jsx
@@ -1,8 +1,8 @@
 import CourseList from '../components/CourseList';
 export default function CoursePage() {
   return (
-    <div className='h-250 mb-40'>
-        <CourseList />
+    <div className="min-h-[calc(100vh-128px)] mb-40">
+      <CourseList />
     </div>
   );
 }
